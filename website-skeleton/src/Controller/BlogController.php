c147dbe7796d358fa0c43e2d4f4c242672a2faf9
Sabri <sabribenbrahim@outlook.com>
2020-12-27 17:01:36 +0100

    Github

---
+++ website-skeleton/src/Controller/BlogController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index(): Response
+    {
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+
+        $articles = $repo->findAll();
+
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+            'articles' => $articles
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(){
+        return $this->render('blog/home.html.twig' , [
+            'title' => "Bienvenue sur TON site internet !"
+        ]);
+    }
+
+    /**
+     * @Route("/blog/12", name="blog_show")
+     */
+    public function show(){
+        return $this->render('blog/show.html.twig');
+    }
+
+
+}
